287461d95c578a519277f868121c73634cebd0fc
محمد عائض صالح لخرش <s24170111027@hu.edu.ye>
2026-06-21 17:53:59 +0300

    Remove unused JavaScript files and refactor wishlist card component

--- public/themes/basket/js/cart.js
+++
@@ -1,7 +0,0 @@
-let total = 0;
-for (let i = 0; i < cart.length; i++) {
-    total += Number(cart[i][2]);
-}
-
-const totaLPrice= document.getElementById("total-price");
-totaLPrice.textContent = total;
